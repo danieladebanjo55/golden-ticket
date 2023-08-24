80d30d55aeb3723e7228f4f4c676d604891d630f
danieladebanjo55 <supremenftcoder@gmail.com>
2023-08-24 15:46:04 -0700

    docs:community section upload

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       <Algorithm />
       <HowTo />
       <Tokenomics />
-      {/* <Community /> */}
+      <Community />
       {/* <Footer /> */}
     </>
   );
--- src/components/Community/Community.jsx
+++ src/components/Community/Community.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import "./Community.scss";
+import Facebook from "../assets/facebook.svg";
+import Twitter from "../assets/twitter.svg";
+import Website from "../assets/website.png";
 
 export default function Community() {
-  return <div>Community</div>;
+  return (
+    <>
+      <section className="my_container">
+        <h2 className="my_title">Our Golden Community</h2>
+        <div className="community_content">
+          <img src={Facebook} alt="facebook" />
+          <img src={Twitter} alt="twitter" />
+          <img src={Website} alt="website" />
+        </div>
+        <div className="my_footer">
+          <p className="footer_text">
+            All Rights Reservered | Golden Ticket Lottery
+          </p>
+        </div>
+      </section>
+    </>
+  );
 }
