912a1219d3d4a75f76162d29e7c2a37eb489aeaf
danieladebanjo55 <supremenftcoder@gmail.com>
2023-08-23 20:31:38 -0700

    docs:about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,42 @@
 import React from "react";
+import { motion } from "framer-motion";
+import candyman from "../assets/candy.png";
+import "./About.scss";
 
 export default function About() {
-  return <>About</>;
+  const fadeIn = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 1.5 } },
+  };
+
+  return (
+    <>
+      <section className="about">
+        <div className="left_side">
+          <div className="image_container">
+            <img src={candyman} />
+          </div>
+        </div>
+        <div className="right_side">
+          <h2 className="about-text">About Golden Ticket</h2>
+          <p className="about-subtext">
+            Our core is the $TICKET token, the heart of our algorithmic raffle
+            system. To join, users buy $TICKET tokens. Each purchase gives a 1
+            in 10 shot at a share of the Golden Ticket prize pool, fueled by
+            taxes from token transactions. Our unique system rewards winners and
+            keeps everyone engaged. Every $TICKET token purchase triggers the
+            Golden Ticket Telegram bot. If a user wins, the bot posts the Golden
+            Ticket image in our Telegram group with prize details, the current
+            pool, and a transaction link. Winning ETH is sent directly to their
+            wallet via our smart contract. Winners also keep their $TICKET
+            tokens. Non-winners aren't left out. They still get updates on
+            potential wins, the pool size, and transaction links. Plus, they
+            retain their $TICKET tokens, which they can hold or sell as they
+            wish.
+          </p>
+          <button className="learn-more-button">Learn More</button>
+        </div>
+      </section>
+    </>
+  );
 }
